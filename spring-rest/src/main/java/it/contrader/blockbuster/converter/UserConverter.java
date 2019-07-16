4529a44acd75a0ac49eadc57f33505fbe6802160
Opolacc <maurizio-94@live.it>
2019-07-16 16:19:29 +0200

    converter

---
+++ spring-rest/src/main/java/it/contrader/blockbuster/converter/UserConverter.java
@@ -0,0 +1,8 @@
+package it.contrader.blockbuster.converter;
+
+import it.contrader.blockbuster.dto.UserDTO;
+import it.contrader.blockbuster.model.User;
+
+public interface UserConverter extends Converter<UserDTO, User> {
+
+}
